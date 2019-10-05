866dc510d47f506c0d1eeb476aa69b507934fffa
James Haughey <jimmyhaugh@googlemail.com>
2019-10-05 17:15:39 +0100

    ENH: add cognito_pool details to config

--- gladnews/static/js/config.js
+++ gladnews/static/js/config.js
@@ -1,6 +1,6 @@
 const cognitoSettings = {
-  UserPoolId: 'us-east-1_DKIcwoZGG',
-  ClientId: '5uectjtrih1d6l2fkq0pooevbj'
+  UserPoolId: 'us-east-1_nI6CYAt9a',
+  ClientId: '5u7dtnav8pa5a6kqtjimmhsrra'
 }
 
 const apiGPrefix = 'https://s17drljvo9.execute-api.us-east-1.amazonaws.com/dev/'
