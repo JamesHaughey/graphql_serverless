9d04df0bc75b150ee90df74e1f41b39d404bd9e7
James Haughey <jimmyhaugh@googlemail.com>
2019-10-06 16:33:15 +0100

    CLN: move config values to const

--- gladnews/static/js/config.js
+++ gladnews/static/js/config.js
@@ -1,6 +1,28 @@
+// function getParameter(paramName) {
+//   let ssm = new AWS.SSM();
+//   let params = {
+//       Name: paramName,
+//       WithDecryption: true
+//   };
+
+//   return new Promise((resolve, reject) => {
+//       ssm.getParameter(params, (err, data) => {
+//           if (err) {
+//               reject(err);
+//           } else {
+//               resolve(data.Parameter['Value']);
+//           }
+//       });
+//   });
+// }
+
+const poolId = 'us-east-1_j03Ole3ui'
+
+const clientId = '37efg02gahf33jj7fsp0ai6fvt'
+
 const cognitoSettings = {
-  UserPoolId: 'us-east-1_nI6CYAt9a',
-  ClientId: '5u7dtnav8pa5a6kqtjimmhsrra'
+  UserPoolId: poolId,
+  ClientId: clientId
 }
 
 const apiGPrefix = 'https://s17drljvo9.execute-api.us-east-1.amazonaws.com/dev/'
